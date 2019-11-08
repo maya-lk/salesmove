3c9ae63a1728093bc111329bac20311ba6641d24
hettipower <hettipower@gmail.com>
2019-11-08 16:31:31 +0530

    Updated profile functions

--- src/components/profile/profile.component.jsx
+++ src/components/profile/profile.component.jsx
@@ -1,11 +1,44 @@
 import React from 'react';
+import Select from 'react-virtualized-select';
 
 import './profile.styles.scss';
 
-const Profile = () => (
-    <div className="accountContent">
-        <div className="titleBar">Profile</div>
-    </div>
-);
+
+class Profile extends React.Component {
+    render(){
+        return(
+            <div className="accountContent">
+                <div className="titleBar">Profile</div>
+            </div>
+        )
+    }
+}
+
+function CountryOptionRenderer ({ focusedOption, focusedOptionIndex, focusOption, key, labelKey, option, options, selectValue, style, valueArray, valueKey }) {
+  
+    return (
+        <div
+            key={key}
+            onClick={() => selectValue(option)}
+            onMouseEnter={() => focusOption(option)}
+            style={{ padding : '0.5rem' , cursor : 'pointer' }}
+        >   
+            
+            <label>
+                {
+                    (option.flagPath)?
+                    (<img
+                        className="countryIcon"
+                        src={option.flagPath}
+                        style={{ width : '30px' , marginRight : '10px' }}
+                        alt={option.value}
+                    />)
+                    : ''
+                } 
+                {option.value}
+            </label>
+        </div>
+    )
+}
 
 export default Profile;
