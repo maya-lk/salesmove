622f5138efac8c770f612472481a5873608792c5
hettipower <hettipower@gmail.com>
2019-11-15 08:32:49 +0530

    Update my ads component with pending and published advertisements

--- src/components/my-ads/my-ads.component.jsx
+++ src/components/my-ads/my-ads.component.jsx
@@ -5,24 +5,41 @@ import { createStructuredSelector } from 'reselect';
 import { selectMyAds } from '../../redux/user/user.selectors';
 
 import MyAdItem from '../my-ad-item/my-ad-item.component';
-import SearchItemLoader from '../content-loader/content-loader.component';
 
 import './my-ads.styles.scss';
 
-const MyAds = ({ myAds }) => (
-    <div className="accountContent">
-        <div className="titleBar">My Advertisements</div>
-        <div className="myAdsWrap">
-            {
-                (myAds)?
-                myAds
-                .filter( ad => ad.status === 'publish' )
-                .map( ad => <MyAdItem key={ad.ID} item={ad} /> )
-                : <SearchItemLoader/>
-            }
+const MyAds = ({ myAds }) => {
+
+    const publishedAdFilter = myAds.filter( ad => ad.status === 'publish' );
+    const pendingAdFilter = myAds.filter( ad => ad.status === 'pending' );
+
+    console.log(publishedAdFilter);
+
+    return(
+        <div className="accountContent">
+            <div className="titleBar">My Advertisements</div>
+            <div className="myAdsWrap">
+                <div className="advertisementWrap">
+                    <h4 className="subTitle">Published Ad</h4>
+                    {
+                        (publishedAdFilter.length)?
+                        publishedAdFilter.map( ad => <MyAdItem key={ad.ID} item={ad} /> )
+                        : <h2 className="noResults">No Published Advertisements</h2>
+                    }
+                </div>
+
+                <div className="advertisementWrap">
+                    <h4 className="subTitle">Pending Ad</h4>
+                    {
+                        (pendingAdFilter.length)?
+                        pendingAdFilter.map( ad => <MyAdItem key={ad.ID} item={ad} /> )
+                        : <h2 className="noResults">No Pending Advertisements</h2>
+                    }
+                </div>
+            </div>
         </div>
-    </div>
-);
+    )
+};
 
 const mapStateToProps = createStructuredSelector({
     myAds : selectMyAds,
