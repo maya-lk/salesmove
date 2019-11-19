9f2693a2fa6afbb811d1ba0db7792a0fe99f55e7
hettipower <hettipower@gmail.com>
2019-11-19 14:05:10 +0530

    Style update PaymentsList component

--- src/components/paymets-list/paymets-list.component.jsx
+++ src/components/paymets-list/paymets-list.component.jsx
@@ -15,16 +15,20 @@ const PaymentsList = ({ payments }) => (
             (<h4>Payment List</h4>)
             : ''
         }
-        {
-            (payments)?
-            payments.map(payment => <div key={payment.ID} className="payment">
-                <ReactFancyBox
-                thumbnail={payment.slip}
-                image={payment.slip}/>
-                <div className="date">{payment.date}</div>
-            </div>)
-            : ''
-        }
+        <div className="paymentsListsWrap d-flex flex-wrap row">
+            {
+                (payments)?
+                payments.map(payment => <div key={payment.ID} className="paymentOuter col-md-4 col-12">
+                    <div className="payment">
+                        <ReactFancyBox
+                        thumbnail={payment.slip}
+                        image={payment.slip}/>
+                        <div className="date">{payment.date}</div>
+                    </div>
+                </div>)
+                : ''
+            }
+        </div>
     </div>
 );
 
